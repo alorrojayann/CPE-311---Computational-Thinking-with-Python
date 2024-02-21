6b618dac13b32bb6cd12041b5d55421685431aa8
Jay-ann Alorro <157359579+alorrojayann@users.noreply.github.com>
2024-02-21 09:20:38 +0800

    toh problem solved

--- Hands-on Activity 4.1: The Tower of Hanoi Problem/TOH.py
+++ Hands-on Activity 4.1: The Tower of Hanoi Problem/TOH.py
@@ -1,1 +1,12 @@
+def toh(n, source, auxiliary, destination):
+  # base case
+  if n == 1:
+    print('Disk', n, 'FROM:', source, '-->', destination)
+  else:
+    toh(n-1, source, destination, auxiliary) # recursive call (source to auxiliary, destination is temporary)
+    print('Disk', n, 'FROM:', source, '-->', destination)
+    toh(n-1, auxiliary, source, destination) # recursive call (auxiliary to destination, source is temporary)
 
+# example use
+n = 3
+toh(n, 'Source', 'Auxiliary', 'Destination')
